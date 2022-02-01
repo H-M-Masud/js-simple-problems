b8a9f4117a25da548c10c5c19d55f05a239425a4
H-M-Masud <hmmasud163@gmail.com>
2022-02-01 10:39:31 +0600

    added sum

---
+++ sum.js
@@ -0,0 +1,37 @@
+/* const numbers = [44, 23, 534, 32, 54, 5, 78];
+let sum = 0;
+for(var i = 0; i < numbers.length; i++){
+    const element = numbers[i];
+    console.log(element)
+    sum = sum + element;
+}
+console.log(sum) */
+/* 
+
+const numbers = [44, 23, 534, 32, 54, 5, 78];
+function arrayTotal(numbers){
+    let sum = 0;
+    for(var i = 0; i < numbers.length; i++){
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+
+const result = arrayTotal([50, 100, 20, 342, 45, 4532, 45, 45, 345])
+
+ */
+
+/* 
+function wordConnect(words){
+    let word = '';
+    for(var i = 0; i < words.length; i++){
+        const item = words[i];
+        word = word + item;
+    }
+    return word;
+}
+
+const sentence = ['a', 'p', 'p', 'l', 'e'];
+const connect = wordConnect(sentence)
+console.log(connect) */
