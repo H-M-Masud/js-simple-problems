00c6c9b3b0573af147f00065ce046dac7afeab43
H-M-Masud <hmmasud163@gmail.com>
2022-02-01 09:23:40 +0600

    added swap

---
+++ swap.js
@@ -0,0 +1,33 @@
+/* var first = 5;
+var second = 7;
+console.log(first, second)
+var temp = first;
+first = second;
+second = temp;
+
+
+console.log(first, second) */
+
+// swap with string
+
+
+/* 
+var fname = "H.M";
+var lname = "Maud";
+console.log(fname, lname)
+
+var temp = fname;
+fname = lname;
+lname = temp;
+console.log(fname, lname) */
+
+
+// swap with simple aways
+/* 
+var first = 3;
+var last = 5;
+console.log(first, last);
+[first, last] = [last, first];
+console.log(first, last)
+
+ */
